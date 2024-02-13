3fff833a965262bef68f40bc77eef6f7fdeb993f
apple1914 <lionpay7@mail.ru>
2024-02-13 08:27:46 +0600

    finished

--- pages/api/convert.js
+++ pages/api/convert.js
@@ -42,8 +42,13 @@ export default async function handler(req, res) {
         const price = prices[priceKey];
 
         const {value} = toCurrencyDoc
+        const TOTAL_FEE = 0.06 - Number(discount)
 
-        const answer = Number(amount) * (1-0.06 + Number(discount)) * value / price
+        const answer = Number(amount) * (1-TOTAL_FEE) * value / price
+
+        console.log("intermediate values in convert are:", {
+            amount,price,value,TOTAL_FEE,answer
+        })
         return res.json(answer)
     
         } catch (e) {
