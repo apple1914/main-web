f32929ffcecc8df847457b127194a1bba801672f
apple1914 <lionpay7@mail.ru>
2024-02-23 09:30:08 +0600

    transak explaienr added but now just need transaltions

---
+++ components/Common/TransakExplainerWhyCrypto.js
@@ -0,0 +1,58 @@
+"use client";
+// import { useTranslation } from "@/app/i18n/client";
+
+import { useState, useEffect } from "react";
+import { Modal } from "react-bootstrap";
+import { addWithdrawalAddress } from "../../backend/requests";
+import { OFFRAMPER_WIDGET_URL } from "../../utils/importantUrls";
+import { useTranslation } from "next-i18next";
+
+export default function TransakExplainerWhyCrypto() {
+
+  const { t } = useTranslation("common");
+  const [show, setShow] = useState(false);
+  const handleClose = () => {
+    
+    setShow(false)
+  }
+  const handleShow = () => {
+    setShow(true);
+  };
+
+  
+
+  return (
+    <>
+      <button 
+        className="btn text-primary mx-1 w-100 mx-auto"
+        onClick={handleShow}
+      >
+        {t("transakExplainerWhyCrypto.button")}
+      </button>
+
+      <Modal show={show} onHide={handleClose} centered>
+        <div className="modal-header">
+          <h5 className="modal-title fw-400">{t("transakExplainerWhyCrypto.title")}</h5>
+         
+        </div>
+        <div className="modal-body p-0 bg-white rounded px-3">
+          <h6 className="modal-title fw-400">
+            {t("transakExplainerWhyCrypto.p1")}
+          </h6>
+          <h6 className="modal-text fw-400">
+            {t("transakExplainerWhyCrypto.p2")}
+          </h6>
+          <button
+                type="button"
+                className="btn btn-primary w-100 mx-auto mx-10 my-3"
+                onClick={(e)=>{
+                  e.preventDefault()
+                  handleClose()
+                }}
+              >{t("Close")}</button>
+        </div>
+        {/* </div> */}
+      </Modal>
+    </>
+  );
+}
