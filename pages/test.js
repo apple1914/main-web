60f8291aacb1b6402a4642820f8519f7651726f8
apple1914 <lionpay7@mail.ru>
2024-03-15 12:18:17 +0600

    sdf

--- pages/test.js
+++ pages/test.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import NavbarTwo from '../components/_App/NavbarTwo';
+import React from "react";
+import NavbarTwo from "../components/_App/NavbarTwo";
 
-import Footer from '../components/_App/Footer';
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import BillingInfo from '../components/Common/BillingInfo';
+import Footer from "../components/_App/Footer";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import BillingInfo from "../components/Common/BillingInfo";
 
 const Test = () => {
-    return (
-        <>
-            {/* <NavbarTwo /> */}
+  return (
+    <>
+      {/* <NavbarTwo /> */}
 
-           <BillingInfo/>
-            
-            {/* <Footer /> */}
-        </>
-    )
-}
+      <BillingInfo />
+
+      {/* <Footer /> */}
+    </>
+  );
+};
 
 export default Test;
 
+export async function getServerSideProps(context) {
+  // extract the locale identifier from the URL
+  const { locale } = context;
 
-
-export async function getStaticProps(context) {
-    // extract the locale identifier from the URL
-    const { locale } = context
-  
-    return {
-      props: {
-        // pass the translation props to the page component
-        ...(await serverSideTranslations(locale)),
-      },
-    }
-  }
+  return {
+    props: {
+      // pass the translation props to the page component
+      ...(await serverSideTranslations(locale)),
+    },
+  };
+}
