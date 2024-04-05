5743f112a53c95d3f7c691fa3631b0b0339863f4
apple1914 <lionpay7@mail.ru>
2024-04-05 09:22:58 +0500

    wa support quick debug

--- pages/payment/mercuryo.js
+++ pages/payment/mercuryo.js
@@ -7,7 +7,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import WhatsappButton from "../../components/Common/WhatsappButton";
 import { getOnDutyCustomerSupportNumber } from "../../lib/customerSupport";
 
-const Payment = ({ props }) => {
+const Payment = (props) => {
   const { customerSupportPhoneNumber } = props;
   return (
     <>
@@ -34,5 +34,6 @@ export async function getStaticProps(context) {
       ...(await serverSideTranslations(locale)),
       customerSupportPhoneNumber: customerSupportPhoneNumber,
     },
+    revalidate: 60,
   };
 }
